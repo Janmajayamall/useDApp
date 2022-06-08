370ccd4ad0821d3649ce682c4817caa86f1021ad
iskanderandrews <iskander.s.andrews@gmail.com>
2022-06-08 05:55:48 +0200

    build: adding reddit arbitrum chain, with writing the testing code

--- packages/testing/dist/src/utils/index.d.ts
+++
@@ -1,7 +0,0 @@
-export * from './deployMulticall';
-export * from './getAdminWallet';
-export * from './IdentityWrapper';
-export * from './mineBlock';
-export * from './waitUntil';
-export * from './waitUtils';
-//# sourceMappingURL=index.d.ts.map
